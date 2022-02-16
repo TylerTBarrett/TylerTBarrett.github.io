916e1e6509cc60c8129c10598eb9b1c88353ee21
Tyler Barrett <tylerbarrett01@gmail.com>
2022-02-16 12:26:33 -0500

    Updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# Portfolio Website
-
-This website gives a quick introduction to who I am and what I've done. Hope you like it!
--- src/library/constants.js
+++
@@ -1,6 +0,0 @@
-const pi = 3.14159;
-const numAnimatedLines = 100;
-const animationLineWidth = 1;
-const animationSpeed = 0.0001;
-
-export { pi, numAnimatedLines, animationLineWidth, animationSpeed };
